b26fe15c1aad3fce81e943d34ae95e5d16b114f6
Ashwani <singalashwani@rediffmail.com>
2020-04-22 17:29:04 +0530

    Added basic components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { AboutComponent } from './about/about.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'article-list', component: ArticleListComponent},
+  {path: 'about', component: AboutComponent},
+  {path: '', component: ArticleListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ArticleListComponent,
+    FooterComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
